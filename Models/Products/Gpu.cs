7a82bf8912e44762c4e132b40c7f59b9e6e5357d
kavitha sundarraj <jeyakumar.kavitha@gmail.com>
2020-11-09 14:37:21 +0100

    second commit. c

--- Models/Products/Gpu.cs
+++ Models/Products/Gpu.cs
@@ -1,10 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+/*OOP Exam – Warehouse management
+ * Gpu is the child class of Product
+ •	Gpu – always has 0.7 weight*/
 
 namespace StorageMaster.Models.Products
 {
-    class Gpu
+    class Gpu : Product
     {
+        public Gpu(Double price, double weight = 0.7) : base(price, weight)
+        {
+            this.Price = price;
+            this.Weight = 0.7;
+        }
     }
 }
--- Models/Products/Harddrive.cs
+++ Models/Products/Harddrive.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+/*OOP Exam – Warehouse management
+ * Harddrive is the child class of Product
+ •	Harddrive – always has 1 weight*/
 namespace StorageMaster.Models.Products
 {
-    class Harddrive
+    class Harddrive : Product
     {
+        public Harddrive(Double price, double weight = 1) : base(price, weight)
+        {
+            this.Price = price;
+            this.Weight = 1;
+        }
     }
 }
