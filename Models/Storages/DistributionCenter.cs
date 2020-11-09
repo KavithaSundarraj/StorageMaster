076d49fdfc669a3123814c7105652f12039927de
Daryl <ng.daryl@gmail.com>
2020-11-09 16:00:04 +0100

    Modified Storage

--- Models/Storages/DistributionCenter.cs
+++ Models/Storages/DistributionCenter.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Runtime.CompilerServices;
 
 namespace StorageMaster.Models.Storages
 {
-    class Truck
+    class DistributionCenter: Storage
     {
+        public DistributionCenter(string name, int capacity, int garageSlots, IEnumerable<Vehicle> vehicles) : base(name, capacity, garageSlots, vechicles)
+        {
+            this.Name = name;
+            this.Capacity = 1;
+            this.GarageSlots = garageSlots;
+        }
     }
 }
