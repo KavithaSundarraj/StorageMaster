1bbfa8185dc68632cee94a7caa033abe513a432a
Sahanadhilfarsirajudeen <71212093+Sahanadhilfarsirajudeen@users.noreply.github.com>
2020-11-09 16:54:10 +0100

    Update Warehouse.cs

--- Models/Storages/Warehouse.cs
+++ Models/Storages/Warehouse.cs
@@ -4,7 +4,13 @@ using System.Text;
 
 namespace StorageMaster.Models.Storages
 {
-    class Vehicle
-    {
-    }
+    class Warehouse : Storage
+    {
+        public Warehouse(string name, int capacity, int garageSlots, IEnumerable<Vehicle> vehicles) : base(name, capacity, garageSlots, vehicles)
+        {
+            this.Name = name;
+            this.Capacity = 10;
+            this.GarageSlots = garageSlots;
+        }
+    }
 }
